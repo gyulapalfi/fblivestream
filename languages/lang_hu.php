eb3f98875db6404a957cb4f55cdfccb860479c67
gyulapalfi <gyula.palfi@gmail.com>
2016-06-16 07:11:24 +0000

    First commit

---
+++ languages/lang_hu.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Default language file
+English
+*/
+$lang = array();
+
+$lang['PAGE_TITEL']         = 'Online, ahogy Einstein csinálná';
+$lang['PAGE_SUBTITLE']      = 'Élő közvetítés a Facebookon';
+
+//Have Acces
+
+
+// No Access
+$lang['NO_ACCES_WARNING']   = 'You Have No Right To Access This Service!';
+$lang['HAVE_SUBSRIPTION']   = 'If you <b>have a live subscription</b>, use the <u>original URL only</u>!<br>
+                                Click this button and try again!';
+
+
+?>
